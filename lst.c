c9db3edcfa54a0d1573bca32f41b84fc0648857a
RodrigoLorenzzonBasso <rodrigolorenzzonbasso@gmail.com>
2019-08-29 09:36:48 -0300

    inserindo numero de execucoes por tarefa

--- lst.c
+++ lst.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 //Testando ssh key
 
@@ -13,30 +14,63 @@ typedef struct {
 } TAREFA;
 
 int main() {
+
   TAREFA tarefas[MAX_N];
-  int n, t, i;
+  int n_tarefas, t, i;
   char grade[MAX_T+1];
   int num_preemp, num_trocas_cont;
 
+  //auxiliares
+  int slack_t[MAX_N];
+
+
   while (1) {
     // LEITURA
-    scanf("%d%d", &n, &t);
-    if (n==0 || t==0)
+    scanf("%d%d", &n_tarefas, &t);
+    if (n_tarefas==0 || t==0)
        break;
-    for (i=0; i<n; ++i) {
+    for (i=0; i<n_tarefas; ++i) {
        tarefas[i].id = 'A'+i;
        scanf("%d%d%d", &(tarefas[i].c), &(tarefas[i].p), &(tarefas[i].d) );
     }
     // SIMULACAO
     num_preemp = 0;
     num_trocas_cont = 0;
-    grade[0] = '.';
+
+    int n_execucoes[n_tarefas];
+
+    for(int j = 0; j < n_tarefas; j++)
+    {
+      n_execucoes[j] = ceil((float)t/(float)tarefas[j].p);
+    }
+
+    
+
+
+
+
+
+    grade[0] = tarefas[0].id;
     grade[1] = '.';
     grade[2] = '.';
     grade[3] = '\0';
+
+    /*for(int j = 0; j < t; j++)
+    {
+      for(int k = 0; k < n_tarefas; k++)
+      {
+        slack_t[k] = d_abs[k] - t_abs - comp_restante[k];
+      }
+    }*/
+
+
     // ...
     // RESULTADOS
     printf("%s\n%d %d\n\n", grade, num_preemp, num_trocas_cont);
   }
   return 0;
 }
+
+//comp_restante = computacoes - (tempo_absoluto - tempo_inicio)
+// ou comp_restante = computacoes - comp_realizadas
+//slack = (deadline_absoluto - tempo_absoluto - comp_restante)
