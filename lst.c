9f806ee7b84afd0ed71a711637e9056797f1e828
RodrigoLorenzzonBasso <rodrigolorenzzonbasso@gmail.com>
2019-09-18 14:38:31 -0300

    Refatoracao do codigo, funcionando para todos os scripts de teste

--- lst.c
+++ lst.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 #define MAX_N 26
 #define MAX_T 2048
@@ -38,53 +39,48 @@ int main() {
     int deadline_abs[n_tarefas];
     int periodo_abs[n_tarefas];
     int comp_abs[n_tarefas];
+
     char completa[n_tarefas];
     for(int j = 0; j < n_tarefas; j++)
       completa[j] = 0;
+    
     int n_completas = 0;
     int menor_slack = MAX_T;
     int tarefa_menor_slack = 0;
 
-    TAREFA anterior;
-    int indice_da_anterior;
-
-    //execucao de cada tempo //
-    for(int j = 0; j < t; j++)
+    // execucao de cada tempo + 1 //
+    for(int j = 0; j < t + 1; j++)
     {
       // execucao para cada tarefa // 
       for(int k = 0; k < n_tarefas; k++)
       {
-        // 1. Antes de Executar as tarefas, verificar as informacoes de cada tarefa
-
-        // comecou a execucao, setar primeiros deadlines e periodos
+        
+        // Primeira execucao, resetar as informacoes absolutas das tarefas
         if(j == 0) 
         {
           periodo_abs[k] = tarefas[k].p;
           deadline_abs[k] = tarefas[k].d;
           comp_abs[k] = 0;
         }
-        // reset do periodo; setar novos deadlines, periodos e computacoes absolutas
-        else if((j % tarefas[k].p == 0) && (completa[k] == 1))
+
+        // Testar se a tarefa esta completa
+        if(((tarefas[k].c - comp_abs[k]) == 0) && completa[k] != 1)
+        {
+          completa[k] = 1;
+          n_completas++;
+        }
+
+        // Reset dos absolutos se a tarefa foi completa
+        if((completa[k] == 1) && (j % tarefas[k].p == 0))
         {
           periodo_abs[k] = ((j / tarefas[k].p)+1) * tarefas[k].p;
           deadline_abs[k] = (((j / tarefas[k].p)+1) * tarefas[k].p) - (tarefas[k].p - tarefas[k].d);
           comp_abs[k] = 0;
           completa[k] = 0;
-        }
-        // caso a tarefa perca o prazo
-        else if((completa[k] == 1) && (deadline_abs[k] < j))
-        {
-          periodo_abs[k] = ((j / tarefas[k].p)+1) * tarefas[k].p;
-          deadline_abs[k] = (((j / tarefas[k].p)+1) * tarefas[k].p) - (tarefas[k].p - tarefas[k].d);
-          comp_abs[k] = 0;
-          completa[k] = 0;
+          n_completas--;
         }
 
-        ///////////////////////////////////////////////////////////////////////////
-
-        // 2. Calculo para saber qual tarefa vai executar
-
-        // se a tarefa ainda nao foi concluida, calcular tempo de slack
+        // se ela nao foi, calcular o slack
         if(completa[k] == 0)
         {    
           int slack_aux = (deadline_abs[k] - j) - (tarefas[k].c - comp_abs[k]);
@@ -103,116 +99,55 @@ int main() {
             }
           }
         }
-        // se foi concluida, aumenta numero de completas (se (completas == n_tarefas) entao o idle ganha o processador)
-        else
-        {
-          n_completas++;
-        }
+      } // execucao para cada tarefa
 
-        ////////////////////////////////////////////////////////////////////////////////
-
-      } // para cada tarefa (k)
-
-      // 3. Calculado o menor slack, executar
-
-      // concluir uma computacao da tarefa com menor slack
+      // concluir uma computacao da tarefa com menor slack se tiver alguma ainda nao completa
       if(n_completas != n_tarefas)
       {
-        comp_abs[tarefa_menor_slack]++;
-
-        // testar se a tarefa foi concluida
-        if((tarefas[tarefa_menor_slack].c - comp_abs[tarefa_menor_slack]) == 0)
-          completa[tarefa_menor_slack] = 1;
-
         char id = tarefas[tarefa_menor_slack].id;
 
-        // caso a tarefa perca o prazo, ela vai ser exibida em minuscula
-        if(deadline_abs[tarefa_menor_slack] < j)
+        // o slack da tarefa que vai executar e negativo, portanto ela perdeu o prazo
+        if(menor_slack < 0)
         {
-          id = id + 32;
+          id = tolower(id);
         }
 
+        comp_abs[tarefa_menor_slack]++;
         grade[j] = id;
-
-        // caso estejamos executando uma tarefa e anteriormente na grade
-        // houve um idle, aumenta o numero de preempcoes
-        if(j>0)
+      }
+      // se nao executa idle
+      else
+      {
+        grade[j] = '.';
+      }
+      
+      // para cada tempo exceto o primeiro, calcular se houve troca de contexto ou preempcao
+      if(j != 0)
+      {
+        // a tarefa que estava executando mudou
+        if(grade[j] != grade[j-1])
         {
+          num_trocas_cont++;
+
+          // calculo do indice da tarefa anterios
+          int id_anterior = toupper(grade[j-1]) - 65;
+
+          // se houve troca de um idle, houve preempcao
           if(grade[j-1] == '.')
           {
             num_preemp++;
           }
-        } 
-      }
-      // todas as tarefas foram concluidas, idle executa
-      else
-      {
-        grade[j] = '.';
-
-        // caso executamos um idle e a execucao anterior n foi um idle tambem
-        // o numero de trocas de contexto aumenta
-        if(grade[j-1] != '.')
-        {
-          num_trocas_cont++;
-        }
-      }
-
-      ////////////////////////////////////////////////////////////////////////////////
-
-      // 4. Calcular trocas de contexto e preempcoes
-
-      // guardar a ultima execucao em uma variavel para descobrir se ha trocas
-      if(j == 0)
-      {
-        anterior = tarefas[tarefa_menor_slack];
-        indice_da_anterior = tarefa_menor_slack;
-      }
-      else
-      {
-        // tarefa anterior e diferente da atual
-        if((anterior.id != tarefas[tarefa_menor_slack].id) && grade[j-1] != '.')
-        {
-          num_trocas_cont++;
-
-          // tarefa anterior e diferente e nao tinha acabado
-          if(anterior.c - comp_abs[indice_da_anterior] != 0)
+          // se houve troca de uma tarefa que ainda n tinha sido completa, houve preempcao
+          else if((completa[id_anterior] == 0) && (j % tarefas[id_anterior].p != 0))
           {
             num_preemp++;
           }
-          anterior = tarefas[tarefa_menor_slack];
-          indice_da_anterior = tarefa_menor_slack;
-        }
-        else if(grade[j-1] == '.')
-        {
-          if(grade[j] != '.')
-          {
-            num_trocas_cont++;
-            anterior = tarefas[tarefa_menor_slack];
-            indice_da_anterior = tarefa_menor_slack;
-          }
         }
       }
 
-      // na ultima execucao, se a tarefa for completada ou estiver em idle, troca_de_contexto++
-      if(j == t-1)
-      {
-        if(completa[tarefa_menor_slack] == 1 || grade[j] == '.')
-        {
-          num_trocas_cont++;
-        }
-
-        if(grade[j] == '.')
-        {
-          num_preemp++;
-        }
-      }
-      
       menor_slack = MAX_T;
-      n_completas = 0;
 
-      ///////////////////////////////////////////////////////////////////////////////////////
-
-    } // para cada tempo (j)
+    } // para cada tempo
 
     grade[t] = '\0';
 
